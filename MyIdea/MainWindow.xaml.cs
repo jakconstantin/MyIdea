44ef341508184b13a384d606c73bc4ec5fdcf4d9
jakconstantin <jakconstantin@gmail.com>
2023-06-29 20:56:54 +0300

    Add new text

--- MyIdea/MainWindow.xaml.cs
+++ MyIdea/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace MyIdea
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("One branch test my new");
+            MessageBox.Show("New text");
         }
     }
 }
