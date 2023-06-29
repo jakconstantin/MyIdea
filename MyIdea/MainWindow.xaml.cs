62787cdcdf8d1c0f724f0a9c1de3cf8cb6446914
jakconstantin <jakconstantin@gmail.com>
2023-06-29 19:54:00 +0300

    One Step

--- MyIdea/MainWindow.xaml.cs
+++ MyIdea/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MyIdea
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("one step");
+        }
     }
 }
