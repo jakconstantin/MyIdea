bfea16c477535fcec38a87eef45de41f69289798
jakconstantin <jakconstantin@gmail.com>
2023-07-03 17:20:29 +0300

    Testing Selenium

--- Engine/Utils.cs
+++ Engine/Utils.cs
@@ -1,6 +1,6 @@
 ﻿namespace Engine
 {
-    public class Class1
+    public class Utils
     {
 
     }
---
+++ MyIdeaTestSelenium/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyIdeaTestSelenium
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://youla.ru/sankt-peterburg";
+        }
+    }
+}
